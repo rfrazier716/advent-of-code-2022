8fa67d223097998fee6a75d31c70849c84bb174b
Ryan <rfrazier716@gmail.com>
2022-12-03 14:23:51 -0500

    Cleaned up Day 2. Leverages Interfaces and is a bitmore concise.

--- day_02/main.go
+++ day_02/main.go
@@ -7,7 +7,14 @@ import (
 	"strings"
 )
 
-type RpsHand int // A rock paper scissors hand
+type RpsHand int    // A rock paper scissors hand
+type RpsOutcome int // The outcome, not boolean because you can draw
+
+type Guide struct {
+	// Maybe a bit excessive to make a guide struct instead of just pass the string
+	OpponentAction rune
+	playerAction   rune
+}
 
 const (
 	Rock     RpsHand = 0
@@ -15,113 +22,109 @@ const (
 	Scissors RpsHand = 2
 )
 
-type RpsOutcome int
-
 const (
 	Win  RpsOutcome = 0
 	Loss RpsOutcome = 1
 	Draw RpsOutcome = 2
 )
 
-func (t RpsHand) verses(hand RpsHand) RpsOutcome {
-	switch hand {
-	case t:
-		return Draw
-	case (t + 1) % 3:
-		return Loss
-	default:
-		return Win
-	}
+type GameRound struct {
+	Hand     RpsHand
+	Opponent RpsHand
+	Outcome  RpsOutcome
+	Score    int
 }
 
-type RpsGame struct {
-	playerA RpsHand
-	playerB RpsHand
+type Player interface {
+	// Player is an interface that can play a game round given a guide input
+	Play(guide Guide) GameRound
 }
 
-func Play(game RpsGame) (result RpsOutcome, points int) {
-	result = game.playerA.verses(game.playerB)
-	points += int(game.playerA) + 1
-	switch result {
-	case Win:
-		points += 6
-	case Loss:
-		points += 0
-	case Draw:
-		points += 3
+// A decoder Func is a wrapper that implements the Player interface
+// lets us write a function to decode the guide input and then play around based on that input
+type DecoderFunc func(Guide) (RpsHand, RpsHand)
+
+func (f DecoderFunc) Play(guide Guide) (round GameRound) {
+	player, opponent := f(guide)
+	return NewRound(player, opponent)
+}
+
+func NewRound(player RpsHand, opponent RpsHand) (round GameRound) {
+	round.Hand = player
+	round.Opponent = opponent
+
+	round.Score = int(round.Hand) + 1 // initial score based on what you played
+	switch opponent {
+	case player:
+		// draw
+		round.Outcome = Draw
+		round.Score += 3
+	case (player + 1) % 3:
+		// Loss
+		round.Outcome = Loss
+	case (player - 1) % 3, 2:
+		// Victory
+		round.Score += 6
+		round.Outcome = Win
 	}
+
 	return
 }
 
-func IntoGuidePartA(input string) (game RpsGame, err error) {
-	// we need our second person to be playerA for the game struct to work
-	switch input[0] {
+func PartOneDecoder(guide Guide) (player RpsHand, opponent RpsHand) {
+	switch guide.OpponentAction {
 	case 'A':
-		game.playerB = Rock
+		opponent = Rock
 	case 'B':
-		game.playerB = Paper
+		opponent = Paper
 	case 'C':
-		game.playerB = Scissors
+		opponent = Scissors
 	}
 
-	switch input[2] {
+	switch guide.playerAction {
 	case 'X':
-		game.playerA = Rock
+		player = Rock
 	case 'Y':
-		game.playerA = Paper
+		player = Paper
 	case 'Z':
-		game.playerA = Scissors
+		player = Scissors
 	}
 
 	return
 }
 
-func partBGuideCreator() func(string) (RpsGame, error) {
-	// we need our second person to be playerA for the game struct to work
-	losingLUT := map[RpsHand]RpsHand{
-		Rock:     Scissors,
-		Scissors: Paper,
-		Paper:    Rock,
+func PartTwoDecoder(guide Guide) (player RpsHand, opponent RpsHand) {
+	switch guide.OpponentAction {
+	case 'A':
+		opponent = Rock
+	case 'B':
+		opponent = Paper
+	case 'C':
+		opponent = Scissors
 	}
 
-	winningLUT := map[RpsHand]RpsHand{
-		Rock:     Paper,
-		Paper:    Scissors,
-		Scissors: Rock,
-	}
-
-	return func(input string) (game RpsGame, err error) {
-		switch input[0] {
-		case 'A':
-			game.playerB = Rock
-		case 'B':
-			game.playerB = Paper
-		case 'C':
-			game.playerB = Scissors
+	switch guide.playerAction {
+	case 'X':
+		player = (opponent - 1) % 3
+		// need to add looping behaviour to this
+		if player < 0 {
+			player = 2
 		}
 
-		switch input[2] {
-		case 'X':
-			game.playerA = losingLUT[game.playerB]
-		case 'Y':
-			game.playerA = game.playerB
-		case 'Z':
-			game.playerA = winningLUT[game.playerB]
-		}
-
-		return
+	case 'Y':
+		player = opponent
+	case 'Z':
+		player = (opponent + 1) % 3
 	}
+
+	return
 }
 
-var IntoGuidePartB func(string) (RpsGame, error) = partBGuideCreator()
-
-func calculateMaxScore(guide []RpsGame) int {
-	totalScore := 0
-	for i := range guide {
-		_, points := Play(guide[i])
-		totalScore += points
+func CalculateScore(strategyGuide []Guide, playstyle Player) (score int) {
+	for i := range strategyGuide {
+		score += playstyle.Play(strategyGuide[i]).Score
 	}
-	return totalScore
+	return
 }
 
 func main() {
@@ -132,22 +135,16 @@ func main() {
 
 	// parse game
 	lines := strings.Split(string(puzzleInput), "\n")
-	roundsA := make([]RpsGame, 0, len(lines))
-	roundsB := make([]RpsGame, 0, len(lines))
 
-	for i := range lines {
-		if len(lines[i]) > 0 {
-			round, _ := IntoGuidePartA(lines[i])
-			roundsA = append(roundsA, round)
-			round, _ = IntoGuidePartB(lines[i])
-			roundsB = append(roundsB, round)
-		}
+	// Create our Strategy Guide
+	strategyGuide := make([]Guide, len(lines)-1)
+	for i := range strategyGuide {
+		strategyGuide[i] = Guide{rune(lines[i][0]), rune(lines[i][2])}
 	}
 
-	partOne := calculateMaxScore(roundsA)
-	partTwo := calculateMaxScore(roundsB)
+	partOne := CalculateScore(strategyGuide, DecoderFunc(PartOneDecoder))
+	partTwo := CalculateScore(strategyGuide, DecoderFunc(PartTwoDecoder))
 
 	fmt.Printf("Part One Solution: %v\n", partOne)
 	fmt.Printf("Part One Solution: %v\n", partTwo)
-
 }
--- day_02/main_test.go
+++ day_02/main_test.go
@@ -4,31 +4,18 @@ import (
 	"testing"
 )
 
-func TestSomethingSilly(t *testing.T) {
+func TestScoreCalculation(t *testing.T) {
 	testTable := []struct {
-		a        int
-		b        int
+		game     GameRound
 		expected int
-	}{{1, 2, 3}, {2, 3, 5}, {-4, 7, 3}}
-	for i, test := range testTable {
-		if res := test.a + test.b; res != test.expected {
-			t.Errorf("Test index %v failed. Expected %v, got %v", i, test.expected, res)
-		}
-	}
-}
-
-func TestParser(t *testing.T) {
-	testTable := []struct {
-		input    string
-		expected RpsGame
 	}{
-		{"A Y", RpsGame{Rock, Rock}},
-		{"B X", RpsGame{Rock, Paper}},
-		{"C Z", RpsGame{Rock, Scissors}},
+		{NewRound(Rock, Rock), 4},
+		{NewRound(Rock, Paper), 1},
+		{NewRound(Rock, Scissors), 7},
 	}
 	for _, test := range testTable {
-		if actual, _ := IntoGuidePartB(test.input); test.expected != actual {
-			t.Errorf("test failed with input %v. Expected %v, got %v", test.input, test.expected, actual)
+		if test.game.Score != test.expected {
+			t.Errorf("test failed with input %v. Expected %v, got %v", test.game, test.expected, test.game.Score)
 		}
 	}
 
